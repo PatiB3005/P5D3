6eb9beb514ed27afe0124e4e6b5435c77646d633
wihu00 <dominik.nerf@wp.pl>
2025-10-30 21:04:31 +0100

    Tutorial fix

--- app/src/main/java/com/example/smartsenior/MainActivity.java
+++ app/src/main/java/com/example/smartsenior/MainActivity.java
@@ -1,40 +1,98 @@
 package com.example.smartsenior;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import com.example.smartsenior.data.ProfileManager;
+
+import com.example.smartsenior.ui.miniGamesMenu.MiniGamesMenuActivity;
+import com.example.smartsenior.ui.moduleMenu.ModuleMenuActivity;
+import com.example.smartsenior.ui.notifications.NotificationsActivity;
+import com.example.smartsenior.ui.profile.ProfileActivity;
+import com.example.smartsenior.ui.settings.SettingsActivity;
+import com.example.smartsenior.ui.tutorial.TutorialActivity;
+import com.example.smartsenior.ui.tutorial.TutorialActivity1;
+import com.example.smartsenior.ui.wirtualAssistant.WirtualAssistantActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ProfileManager profileManager;
-    private EditText editName;
-    private TextView tvProgress;
-    private Button btnSave, btnLoad;
+    private Button btnModuleMenu, btnProfile, btnMiniGames, btnWirtualAssistant, btnTutorial, btnNotifications, btnSettings, btnExit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_menu);
 
-        profileManager = new ProfileManager(this);
+        btnProfile = findViewById(R.id.btnProfile);
+        btnTutorial = findViewById(R.id.btnTutorial);
+        btnModuleMenu = findViewById(R.id.btnModulesMenu);
+        btnMiniGames = findViewById(R.id.btnMiniGames);
+        btnWirtualAssistant = findViewById(R.id.btnWirtualAssistant);
+        btnNotifications = findViewById(R.id.btnNotifications);
+        btnSettings = findViewById(R.id.btnSettings);
+        btnExit = findViewById(R.id.btnExit);
 
-        editName = findViewById(R.id.editName);
-        tvProgress = findViewById(R.id.tvProgress);
-        btnSave = findViewById(R.id.btnSave);
-        btnLoad = findViewById(R.id.btnLoad);
-
-        btnSave.setOnClickListener(v -> {
-            String name = editName.getText().toString();
-            profileManager.saveProfile(name, 50); // przykładowy postęp
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
         });
 
-        btnLoad.setOnClickListener(v -> {
-            String name = profileManager.getName();
-            int progress = profileManager.getProgress();
-            tvProgress.setText("Użytkownik: " + name + "\nPostęp: " + progress + "%");
+        btnTutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TutorialActivity1.class);
+                startActivity(intent);
+            }
         });
+
+        btnModuleMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ModuleMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMiniGames.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MiniGamesMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnWirtualAssistant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WirtualAssistantActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnNotifications.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NotificationsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnExit.setOnClickListener(v -> {
+            finishAffinity();
+        });
+
     }
 }
