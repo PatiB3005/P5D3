050df3438baae8a890967f67df81cfb1df642cdf
czarnygroch <anastazjafrackowiak2@gmail.com>
2026-01-12 22:31:23 +0100

    Merge origin/miniGamesMenu into main

--- app/src/main/java/com/example/smartsenior/ui/miniGamesMenu/miniGames/MiniGame1Activity.java
+++ app/src/main/java/com/example/smartsenior/ui/miniGamesMenu/miniGames/MiniGame1Activity.java
@@ -5,31 +5,12 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.smartsenior.R;
-import com.example.smartsenior.ui.miniGamesMenu.MiniGamesMenuActivity;
-import com.example.smartsenior.ui.miniGamesMenu.miniGames.MemoryGame.MemoryActivity;
-import com.google.android.material.button.MaterialButton;
-
 public class MiniGame1Activity extends AppCompatActivity {
-
-    private MaterialButton btnPlay;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mini_game_1);
 
-        findViewById(R.id.btnBack).setOnClickListener(v -> {
-            Intent intent = new Intent(MiniGame1Activity.this, MiniGamesMenuActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            startActivity(intent);
-            finish();
-        });
-
-        btnPlay = findViewById(R.id.btnPlay);
-        btnPlay.setOnClickListener(v -> {
-            Intent intent = new Intent(MiniGame1Activity.this, MemoryActivity.class);
-            startActivity(intent);
-        });
+        startActivity(new Intent(this, MemoryActivity.class));
+        finish();
     }
 }
